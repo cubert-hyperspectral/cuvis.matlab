3a1f3f84ac39401db8b39105c67a413c4b65edb2
Benjamin Müller <mueller@cubert-gmbh.de>
2024-01-02 14:50:29 +0100

    Update cuvis_init.m

--- cuvis_init.m
+++ cuvis_init.m
@@ -5,8 +5,6 @@ function cuvis_init(settings_path)
         error('Could not locate CUVIS installation')
     end
 
-    
-    
     if isempty(cuvis_is_initialized)
 
         [filepath,~,~]=fileparts(mfilename('fullpath'));
@@ -14,45 +12,8 @@ function cuvis_init(settings_path)
         addpath(getenv('CUVIS'));
         addpath(fullfile(getenv('CUVIS'),'../','sdk','cuvis_c'));
 
-        % need to copy files to current directory in order to get it running
-        files = dir(fullfile(getenv('CUVIS'),'*.dll'));
-        for k=1:length(files)
-            if ~strcmp(files(k).name,'cuvis.dll')
-                if ~exist([pwd,'/',files(k).name],'file')
-                  copyfile(fullfile(getenv('CUVIS'),files(k).name), pwd);  
-                end
-            end
-        end
-        
-        files = dir(fullfile(getenv('CUVIS'),'*.xml'));
-        for k=1:length(files)
-            if ~strcmp(files(k).name,'cuvis.dll')
-                if ~exist([pwd,'/',files(k).name],'file')
-                  copyfile(fullfile(getenv('CUVIS'),files(k).name), pwd);  
-                end
-            end
-        end
-
-        files = dir(fullfile(getenv('CUVIS'),'*.cti'));
-        for k=1:length(files)
-            if ~strcmp(files(k).name,'cuvis.dll')
-                if ~exist([pwd,'/',files(k).name],'file')
-                  copyfile(fullfile(getenv('CUVIS'),files(k).name), pwd);  
-                end
-            end
-        end
-
-
-        files = dir(fullfile(getenv('CUVIS'),'../','sdk','cuvis_c','cuvis.*'));
-        for k=1:length(files)
-            if ~exist([pwd,'/',files(k).name],'file')
-              copyfile(fullfile(getenv('CUVIS'),'../','sdk','cuvis_c',files(k).name), pwd);  
-            end
-
-        end
-
         if ~libisloaded('cuvis')
-            loadlibrary('cuvis.dll','cuvis.h');
+            loadlibrary(fullfile(getenv('CUVIS'),'../','sdk','cuvis_c','cuvis.dll'),fullfile(getenv('CUVIS'),'../','sdk','cuvis_c','cuvis.h'));
         end
         calllib('cuvis','cuvis_set_log_level',3);
         if ~(exist('settings_path','var'))
